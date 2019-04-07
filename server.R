f2d13a2e50b29b7e2ebda0893a1ed80f9574be3b
Cobrastatistics <nelson.konrad91@gmail.com>
2019-04-07 19:30:52 +0200

    Removed comments in plot

--- server.R
+++ server.R
@@ -30,12 +30,14 @@ server <- function(input, output) {
     pca <- prcomp(dI[,-1], scale. = T, center = T)
 
     
-    ggplot( as.data.frame(pca$x) , aes(x = PC1 , y = PC2 , label = datasetInput()$Country)) + # best?mmer data och variabler att visa
-      geom_hline( yintercept = 0, linetype = 2) + # ritar referenslinje horizontellt
-      geom_vline( xintercept = 0, linetype = 2) + # ritar referenslinje lodr?tt
-      geom_point() + # ritar punkter i diagrammet
-      geom_text_repel() + # separerar labels som ligger ovanp? varandra
-      theme_classic( base_size = 14)  # s?tter l?mpligt tema och grundstorlek p? diagramkomponenter
+    ggplot( as.data.frame(pca$x) , 
+            aes(x = PC1 , y = PC2 , 
+                label = datasetInput()$Country)) +
+                    geom_hline( yintercept = 0, linetype = 2) +
+                    geom_vline( xintercept = 0, linetype = 2) +
+                    geom_point() +
+                    geom_text_repel() +
+                    theme_classic( base_size = 14)
       
     
   })
